d98a44e5cd91e9357c02806389ddff2ec2699a43
Armin Kianian <arminkia.dev@gmail.com>
2019-09-01 19:33:52 +0430

    Updates

---
+++ precache-manifest.7ee3ef542634d79794867f80cdbc1fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1cd7071c8011b5c179ee5f1c917ead",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b418f39eaf5c7ea7be1",
+    "url": "/monsters-rolodex/static/css/main.951e70da.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "9b418f39eaf5c7ea7be1",
+    "url": "/monsters-rolodex/static/js/main.8c6fefea.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
